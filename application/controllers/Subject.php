6b77c10bd088b1bf7f8966c00256f0951efe0deb
Ashish Verma <developeashish708@gmail.com>
2023-11-04 17:17:47 +0530

    subject ajax added

--- application/controllers/Subject.php
+++ application/controllers/Subject.php
@@ -2,6 +2,9 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Subject extends CI_Controller {
+    function index(){
+        $this->load->view("subject/index");
+    }
     function create(){
         if (isset($_POST['submit'])) {
             $this->load->model('Subject_model');
@@ -29,5 +32,30 @@ class Subject extends CI_Controller {
     function delete(){
         $this->load->model('Subject_model');
         $this->Subject_model->del();
+    }
+    
+    function ajax_records(){
+        $this->load->model('Subject_model');
+        $records=$this->Subject_model->records();
+        foreach ($records as $row) {
+            echo "<tr>";
+            echo "<td>" . $row["subid"] . "</td>";
+            echo "<td>" . $row["cshort"] . "</td>";
+            echo "<td>" . $row["cfull"] . "</td>";
+            echo "<td>" . $row["sub1"] . "</td>";
+            echo "<td>" . $row["sub2"] . "</td>";
+            echo "<td>" . $row["sub3"] . "</td>";
+            echo "<td>" . $row["sub4"] . "</td>";
+            echo "<td>" . $row["dt_created"] . "</td>";
+            echo "<td>" . $row["update_date"] . "</td>";
+            echo "<td><a href='update?updateid=" . $row["subid"] . "' class='btn btn-primary'>update</a></td>";
+            echo "<td><a href='delete?deleteid=" . $row["subid"] . "' class='btn btn-danger'>Delete</a></td>";
+            echo "</tr>";
+        }
+    }
+
+    function ajax_create(){
+        $this->load->model('Subject_model');
+        $this->Subject_model->create();
     }
 }
---
+++ application/views/subject/index.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Country </title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <h1>Country Form </h1>
+        <form name="myForm" method="post" id="createCityForm" onsubmit="return addCity();">
+        <div class="mb-3">
+                <label for="cshort" class="form-label">Course Short</label>
+                <input type="number" class="form-control" id="cshort" name="cshort" placeholder="Enter Course Short">
+            </div>
+            <div class="mb-3">
+                <label for="cfull" class="form-label">Course Name</label>
+                <input type="text" class="form-control" id="cfull" name="cfull" placeholder="Enter Course Name">
+            </div>
+            <div class="mb-3">
+                <label for="sub1" class="form-label">Subject first</label>
+                <input type="text" class="form-control" id="sub1" name="sub1" placeholder="Enter Country Subject">
+            </div>
+            <div class="mb-3">
+                <label for="sub2" class="form-label">Subject second</label>
+                <input type="text" class="form-control" id="sub2" name="sub2" placeholder="Enter Country Subject">
+            </div>
+            <div class="mb-3">
+                <label for="sub3" class="form-label">Subject third</label>
+                <input type="text" class="form-control" id="sub3" name="sub3" placeholder="Enter Country Subject">
+            </div>
+            <div class="mb-3">
+                <label for="sub4" class="form-label">Subject fourth</label>
+                <input type="text" class="form-control" id="sub4" name="sub4" placeholder="Enter Country Subject">
+            </div>
+            <div class="mb-3">
+                <label for="dt_created" class="form-label">Date </label>
+                <input type="date" class="form-control" id="dt_created" name="dt_created" placeholder="Enter Date">
+            </div>
+            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+
+        </form>
+        <table class="table">
+            <thead>
+            <tr>
+                    <th scope="col">Sub ID</th>
+                    <th scope="col">C Short</th>
+                    <th scope="col">C Full</th>
+                    <th scope="col">Sub 1</th>
+                    <th scope="col">Sub 2</th>
+                    <th scope="col">Sub 3</th>
+                    <th scope="col">Sub 4</th>
+                    <th scope="col">Dt Created</th>
+                    <th scope="col">Update date</th>
+                    <th scope="col">Update</th>
+                    <th scope="col">Delete</th>
+                </tr>
+            </thead>
+            <tbody id="records">
+            </tbody>
+        </table>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.7.1.js"
+        integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
+    <script>
+    function renderList() {
+        $.ajax({
+            url: "ajax_records",
+            success: function(result) {
+                $("#records").html(result);
+            }
+        });
+    }
+
+    function addCity() {
+        var formdata = $("#createCityForm").serialize();
+        $.ajax({
+            type: "POST",
+            url: "ajax_create",
+            data: formdata,
+            success: function(result) {
+                renderList();
+            }
+        });
+        return false;
+    }
+
+    $(document).ready(function() {
+        renderList();
+    });
+    </script>
+</body>
+
+</html>
