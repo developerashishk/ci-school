61dcf42b0f90fff0c0bdd7153fbf332d9ad5ec06
Arun <developerarunk@gmail.com>
2023-11-08 17:45:35 +0530

    Modal Added for add/edit

--- application/views/city/index.php
+++ application/views/city/index.php
@@ -11,24 +11,57 @@
 
 <body>
     <div class="container">
-        <h1>City Form </h1>
-        <form name="myForm" method="post" id="createCityForm" onsubmit="return addCity();">
-            <div class="mb-3">
-                <label for="name" class="form-label">City Name</label>
-                <input type="text" class="form-control" id="name" name="name" placeholder="Enter City Name">
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+         Add City
+        </button>
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">City Form</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form name="myForm" method="post" id="createCityForm" onsubmit="return addCity();">
+                        <div class="mb-3">
+                            <label for="name" class="form-label">City Name</label>
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Enter City Name">
+                        </div>
+                    
+                        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+                </div>
             </div>
-           
-            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-        </form>
-        <form name="myForm" method="post" id="updateCityForm" style="display:none" onsubmit="return updateCity();">
-            <div class="mb-3">
-                <label for="name" class="form-label">City Name</label>
-                <input type="text" class="form-control" id="update_name" name="name" placeholder="Enter City Name">
+        </div>
+        <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editModalLabel">City Form</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <form name="myForm" method="post" id="updateCityForm" onsubmit="return updateCity();">
+                        <div class="mb-3">
+                            <label for="name" class="form-label">City Name</label>
+                            <input type="text" class="form-control" id="update_name" name="name" placeholder="Enter City Name">
+                        </div>
+                    
+                        <input type="hidden" id="update_id" name="id" />
+                        <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+                </div>
             </div>
-        
-            <input type="hidden" id="update_id" name="id" />
-            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-        </form>
+        </div>
         <table class="table">
             <thead>
                 <tr>
@@ -43,6 +76,7 @@
             </tbody>
         </table>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.js"
         integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
         <script>
@@ -78,7 +112,8 @@
     }
 
     function update(data){
-        document.getElementById("updateCityForm").style.display="block";
+        new bootstrap.Modal(document.getElementById('editModal')).show();
+        // document.getElementById("updateCityForm").style.display="block";
         document.getElementById("update_name").value=data.name;
         document.getElementById("update_id").value=data.id;
     }
@@ -91,7 +126,8 @@
             data: formdata,
             success: function(result) {
                 renderList();
-                document.getElementById("updateCityForm").style.display="none";
+                new bootstrap.Modal(document.getElementById('editModal')).hide();
+                // document.getElementById("updateCityForm").style.display="none";
             }
         });
         return false;
