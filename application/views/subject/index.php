e9380e98dfc3fcbe8c5c882b340f45ec03dac63a
Ashish Verma <developeashish708@gmail.com>
2023-11-09 12:07:44 +0530

    subject table modal added

--- application/views/subject/index.php
+++ application/views/subject/index.php
@@ -11,67 +11,124 @@
 
 <body>
     <div class="container">
-        <h1>Subject Form</h1>
-        <form name="myForm" method="post" id="createCityForm" onsubmit="return addCity();">
-            <div class="mb-3">
-                <label for="cshort" class="form-label">Course Short Number</label>
-                <input type="number" class="form-control" id="cshort" name="cshort" placeholder="Enter Course Short">
-            </div>
-            <div class="mb-3">
-                <label for="cfull" class="form-label">Course Name</label>
-                <input type="text" class="form-control" id="cfull" name="cfull" placeholder="Enter Course Name">
-            </div>
-            <div class="mb-3">
-                <label for="sub1" class="form-label">Subject first</label>
-                <input type="text" class="form-control" id="sub1" name="sub1" placeholder="Enter Course Subject">
-            </div>
-            <div class="mb-3">
-                <label for="sub2" class="form-label">Subject second</label>
-                <input type="text" class="form-control" id="sub2" name="sub2" placeholder="Enter Course Subject">
-            </div>
-            <div class="mb-3">
-                <label for="sub3" class="form-label">Subject third</label>
-                <input type="text" class="form-control" id="sub3" name="sub3" placeholder="Enter Course Subject">
-            </div>
-            <div class="mb-3">
-                <label for="sub4" class="form-label">Subject fourth</label>
-                <input type="text" class="form-control" id="sub4" name="sub4" placeholder="Enter Course Subject">
-            </div>
-            <div class="mb-3">
-                <label for="dt_created" class="form-label">Date </label>
-                <input type="date" class="form-control" id="dt_created" name="dt_created" placeholder="Enter Date">
-            </div>
-            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+        <!-- modal start -->
 
-        </form>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Add Subject
+        </button>
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Subject Form</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form name="myForm" method="post" id="createCityForm" onsubmit="return addCity();">
+                            <div class="mb-3">
+                                <label for="cshort" class="form-label">Course Short Number</label>
+                                <input type="number" class="form-control" id="cshort" name="cshort"
+                                    placeholder="Enter Course Short">
+                            </div>
+                            <div class="mb-3">
+                                <label for="cfull" class="form-label">Course Name</label>
+                                <input type="text" class="form-control" id="cfull" name="cfull"
+                                    placeholder="Enter Course Name">
+                            </div>
+                            <div class="mb-3">
+                                <label for="sub1" class="form-label">Subject first</label>
+                                <input type="text" class="form-control" id="sub1" name="sub1"
+                                    placeholder="Enter Course Subject">
+                            </div>
+                            <div class="mb-3">
+                                <label for="sub2" class="form-label">Subject second</label>
+                                <input type="text" class="form-control" id="sub2" name="sub2"
+                                    placeholder="Enter Course Subject">
+                            </div>
+                            <div class="mb-3">
+                                <label for="sub3" class="form-label">Subject third</label>
+                                <input type="text" class="form-control" id="sub3" name="sub3"
+                                    placeholder="Enter Course Subject">
+                            </div>
+                            <div class="mb-3">
+                                <label for="sub4" class="form-label">Subject fourth</label>
+                                <input type="text" class="form-control" id="sub4" name="sub4"
+                                    placeholder="Enter Course Subject">
+                            </div>
+                            <div class="mb-3">
+                                <label for="dt_created" class="form-label">Date </label>
+                                <input type="date" class="form-control" id="dt_created" name="dt_created"
+                                    placeholder="Enter Date">
+                            </div>
+                            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- modal end -->
+
+        <!-- modal start -->
+        <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="editModalLabel">Subject Form</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form name="myForm" method="post" id="updateCityForm" onsubmit="return updateCity();">
+                            <div class="mb-3">
+                                <label for="update_cfull" class="form-label">Course Name</label>
+                                <input type="text" class="form-control" id="update_cfull" name="cfull"
+                                    placeholder="Enter Course Name">
+                            </div>
+                            <div class="mb-3">
+                                <label for="update_sub1" class="form-label">Subject first</label>
+                                <input type="text" class="form-control" id="update_sub1" name="sub1"
+                                    placeholder="Enter Course Subject">
+                            </div>
+                            <div class="mb-3">
+                                <label for="update_sub2" class="form-label">Subject second</label>
+                                <input type="text" class="form-control" id="update_sub2" name="sub2"
+                                    placeholder="Enter Course Subject">
+                            </div>
+                            <div class="mb-3">
+                                <label for="update_sub3" class="form-label">Subject third</label>
+                                <input type="text" class="form-control" id="update_sub3" name="sub3"
+                                    placeholder="Enter Course Subject">
+                            </div>
+                            <div class="mb-3">
+                                <label for="update_sub4" class="form-label">Subject fourth</label>
+                                <input type="text" class="form-control" id="update_sub4" name="sub4"
+                                    placeholder="Enter Course Subject">
+                            </div>
+
+                            <input type="hidden" id="update_subid" name="subid" />
+                            <button type="submit" name="submit" class="btn btn-primary">Submit</button>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- modal end -->
 
 
 
-        <form name="myForm" method="post" id="updateCityForm" style="display:none" onsubmit="return updateCity();">
-    <div class="mb-3">
-        <label for="update_cfull" class="form-label">Course Name</label>
-        <input type="text" class="form-control" id="update_cfull" name="cfull" placeholder="Enter Course Name">
-    </div>
-    <div class="mb-3">
-        <label for="update_sub1" class="form-label">Subject first</label>
-        <input type="text" class="form-control" id="update_sub1" name="sub1" placeholder="Enter Course Subject">
-    </div>
-    <div class="mb-3">
-        <label for="update_sub2" class="form-label">Subject second</label>
-        <input type="text" class="form-control" id="update_sub2" name="sub2" placeholder="Enter Course Subject">
-    </div>
-    <div class="mb-3">
-        <label for="update_sub3" class="form-label">Subject third</label>
-        <input type="text" class="form-control" id="update_sub3" name="sub3" placeholder="Enter Course Subject">
-    </div>
-    <div class="mb-3">
-        <label for="update_sub4" class="form-label">Subject fourth</label>
-        <input type="text" class="form-control" id="update_sub4" name="sub4" placeholder="Enter Course Subject">
-    </div>
 
-    <input type="hidden" id="update_subid" name="subid" />
-    <button type="submit" name="submit" class="btn btn-primary">Submit</button>
-</form>
+
+
+
+
+
+
 
 
 
@@ -95,6 +152,9 @@
             </tbody>
         </table>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
+    </script>
     <script src="https://code.jquery.com/jquery-3.7.1.js"
         integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script>
@@ -109,7 +169,7 @@
 
     function ajax_del(subid) {
         $.ajax({
-            url: "ajax_del/"+subid,
+            url: "ajax_del/" + subid,
             success: function(result) {
                 renderList();
             }
@@ -129,17 +189,18 @@
         return false;
     }
 
-    function update(data){
-        document.getElementById("updateCityForm").style.display="block";
-        document.getElementById("update_cfull").value=data.cfull;
-        document.getElementById("update_sub1").value=data.sub1;
-        document.getElementById("update_sub2").value=data.sub2;
-        document.getElementById("update_sub3").value=data.sub3;
-        document.getElementById("update_sub4").value=data.sub4;
-        document.getElementById("update_subid").value=data.subid;
+    function update(data) {
+        new bootstrap.Modal(document.getElementById('editModal')).show();
+        // document.getElementById("updateCityForm").style.display="block";
+        document.getElementById("update_cfull").value = data.cfull;
+        document.getElementById("update_sub1").value = data.sub1;
+        document.getElementById("update_sub2").value = data.sub2;
+        document.getElementById("update_sub3").value = data.sub3;
+        document.getElementById("update_sub4").value = data.sub4;
+        document.getElementById("update_subid").value = data.subid;
     }
 
-    function updateCity(){
+    function updateCity() {
         var formdata = $("#updateCityForm").serialize();
         $.ajax({
             type: "POST",
@@ -147,7 +208,8 @@
             data: formdata,
             success: function(result) {
                 renderList();
-                document.getElementById("updateCityForm").style.display="none";
+                new bootstrap.Modal(document.getElementById('editModal')).hide();
+                // document.getElementById("updateCityForm").style.display="none";
             }
         });
         return false;
